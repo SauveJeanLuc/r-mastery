21a9dd313de0255832fb927e0a4cba27b0aaf6da
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-08-15 18:38:07 +0200

    starter file

---
+++ hooks/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import StateComponent from './State';
+export default function App() {
+  return (
+    <>
+        <StateComponent/>
+    </>
+  )
+}
+
+render(<App />, document.getElementById("root"));
