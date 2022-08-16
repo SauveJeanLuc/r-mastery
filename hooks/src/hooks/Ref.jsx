c104424c96b2d71bd8cd509ab4960f1a5b50eec0
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-08-16 17:34:04 +0200

    Accessing DOM Element with useRef

--- hooks/src/hooks/Ref.jsx
+++ hooks/src/hooks/Ref.jsx
@@ -4,7 +4,8 @@ import { useState, useRef } from "react";
 export default function Ref() {
   const [number, setNumber] = useState(0);
   const refNumber = useRef(0);
-
+  const searchRef = useRef();
+  
   function delayLogging(){
     setNumber(number +1);
     refNumber.current++;
@@ -14,7 +15,17 @@ export default function Ref() {
     },1000)
   }
 
+  function focusOnInput(){
+    searchRef.current.focus();
+  }
+
+
   return (
-    <button onClick={delayLogging}> Delay Logging </button>
+    <>
+        <button onClick={delayLogging}> Delay Logging </button>
+        <br />
+        <input type="search" ref={searchRef}/>
+        <button onClick={focusOnInput}> Focus on the input </button>
+    </>
   )
 }
